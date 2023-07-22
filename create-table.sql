c3e60d8c15091bd41b62696f9c4ecb0789720e03
Philani Kemele <128334115+PhilaniKemele@users.noreply.github.com>
2023-07-22 13:24:24 +0200

    Create create-table.sql

---
+++ create-table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Characters (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  role VARCHAR(50) NOT NULL,
+  power_level INT NOT NULL
+);
+
+-- What I've done here is I created a table with the fields: id, name, role, and power_level for Death Note characters. I heard you were an Anime fan
+--
